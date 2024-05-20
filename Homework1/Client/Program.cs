8fe5eeca9ebb1c4ebc11a4cfbd6a2b805ceae98e
IlyTom <126102373+IlyTom@users.noreply.github.com>
2024-05-20 18:49:05 +0300

    Homework1 Single Client-Server

---
+++ Homework1/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваш никнейм:");
+            string nickname = Console.ReadLine()!;
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect("127.0.0.1", 4555);
+            while (true)
+            {
+                Console.WriteLine("Введите сообщение: ");
+                string message = Console.ReadLine()!;
+                Message msg = new Message(message, DateTime.Now, nickname, "Server");
+                string data = msg.Serialize();
+                Byte[] buffer = Encoding.UTF8.GetBytes(data);                
+                udpClient.Send(buffer);
+                //Response
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 4555);
+                udpClient.Client.ReceiveTimeout = 5000;
+                try
+                {
+                    Byte[] bufferResponse = udpClient.Receive(ref remoteEP);
+                    string jsonResponse = Encoding.UTF8.GetString(bufferResponse);
+                    Message? response = Message.Deserialize(jsonResponse);
+                    Console.WriteLine($"[{DateTime.Now}] Ваше сообщение: \"{response.Text}\" доставлено {response.Sender}");
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine($"[{DateTime.Now}] Ваше сообщение не было доставлено!");
+                }
+            }
+        }
+    }
+}
---
+++ Homework1/Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using Client;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient server = new UdpClient(4555);
+            while (true)
+            {
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 4555);
+                Byte[] data = server.Receive(ref remoteEP);
+                string jsonMsg = Encoding.UTF8.GetString(data);
+                Message? message = Message.Deserialize(jsonMsg);
+                Console.WriteLine($"[{message.DateTime}] Получено сообшение: \"{message.Text}\" от {message.Sender}");
+
+                //Response
+                Message response = new Message($"Ваше сообщение: {message.Text} доставлено {message.Receiver}", DateTime.Now, "Server",message.Sender);
+                Byte[] dateResponse = Encoding.UTF8.GetBytes(response.Serialize());
+                server.Send(dateResponse, dateResponse.Length, remoteEP);
+            }
+            
+        }
+    }
+}

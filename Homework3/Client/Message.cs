ce3927cc2a2fc39d27ea199e1e76861c9886c745
IlyTom <126102373+IlyTom@users.noreply.github.com>
2024-05-24 19:34:55 +0300

    Homework3 (async-await + cancellation token)

---
+++ Homework3/Client/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Message
+    {
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public Message(string Text, DateTime DateTime, string Sender, string Receiver) { 
+            this.Text = Text;
+            this.DateTime = DateTime;
+            this.Sender = Sender;
+            this.Receiver = Receiver;
+        }
+
+        public string Serialize() => JsonSerializer.Serialize(this);
+
+        public static Message? Deserialize(string message) => JsonSerializer.Deserialize<Message>(message);
+
+    }
+}
---
+++ Homework3/Client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await StartClient();
+        }
+
+        static async Task StartClient()
+        {
+            bool isRunning = true;
+            Console.WriteLine("Введите Ваш никнейм:");
+            string nickname = Console.ReadLine()!;
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect("127.0.0.1", 4555);
+
+
+            while (isRunning)
+            {
+                Console.WriteLine("Введите сообщение: ");
+                string message = Console.ReadLine()!;
+                if (message.ToLower().Equals("exit"))
+                {
+                    isRunning = false;
+                    udpClient.Close();
+                }
+                else
+                {
+                    Message msg = new Message(message, DateTime.Now, nickname, "Server");
+                    string data = msg.Serialize();
+                    Byte[] buffer = Encoding.UTF8.GetBytes(data);
+                    await udpClient.SendAsync(buffer);
+                    
+                    await ReceiveMessage(udpClient);                    
+                }
+            }
+        }
+        
+        static async Task ReceiveMessage(UdpClient udpClient)
+        {
+            var remoteEP = await udpClient.ReceiveAsync();            
+            try
+            {
+                Byte[] bufferResponse = remoteEP.Buffer;
+                string jsonResponse = Encoding.UTF8.GetString(bufferResponse);
+                Message? response = Message.Deserialize(jsonResponse);
+                Console.WriteLine($"[{DateTime.Now}] Ваше сообщение: \"{response.Text}\"");
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine($"[{DateTime.Now}] Ваше сообщение не было доставлено!");
+            }
+        }
+    }
+}

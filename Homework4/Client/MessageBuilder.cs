31a1c4702c26b826251d6d68bf81ba211c758955
IlyTom <126102373+IlyTom@users.noreply.github.com>
2024-05-28 18:16:28 +0300

    Homework4 add Builder

---
+++ Homework4/Client/MessageBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class MessageBuilder
+    {
+        private Message _message = new Message();
+
+        public MessageBuilder SetSender(string sender)
+        {
+            _message.Sender = sender;
+            return this;
+        }
+        public MessageBuilder SetReceiver(string receiver)
+        {
+            _message.Receiver = receiver;
+            return this;
+        }
+        public MessageBuilder SetText(string text)
+        {
+            _message.Text = text;
+            return this;
+        }
+        public MessageBuilder SetDateTime(DateTime dateTime)
+        {
+            _message.DateTime = dateTime;
+            return this;
+        }
+
+        public Message Build()
+        {
+            return _message;
+        }
+    }
+}
---
+++ Homework4/Server/Program.cs
@@ -0,0 +1,57 @@
+﻿using Client;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Server
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var serverTask = StartServer(token);
+            Console.WriteLine("Нажмите любую кнопку для завершения работы сервера...");
+            Console.ReadKey();
+            cts.Cancel();  
+            
+            try
+            {             
+                await serverTask;                
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Сервер остановлен.");
+            }
+        }
+
+        static async Task StartServer(CancellationToken cts)
+        {
+            UdpClient server = new UdpClient(4555);
+            while (!cts.IsCancellationRequested)
+            {   
+                           
+                var remoteEP = await server.ReceiveAsync(cts);
+                
+                string jsonMsg = Encoding.UTF8.GetString(remoteEP.Buffer);
+                Message? message = Message.Deserialize(jsonMsg);
+                Console.WriteLine($"[{message.DateTime}] Получено сообшение: \"{message.Text}\" от {message.Sender}");
+
+                //Response
+                Message response = new MessageBuilder()
+                        .SetText($"Ваше сообщение: {message.Text} доставлено {message.Receiver}")
+                        .SetDateTime(DateTime.Now)
+                        .SetSender("Server")
+                        .SetReceiver(message.Sender)
+                        .Build();
+                Byte[] dateResponse = Encoding.UTF8.GetBytes(response.Serialize());
+                await server.SendAsync(dateResponse, dateResponse.Length, remoteEP.RemoteEndPoint);
+
+                
+            }
+        }
+        
+    }
+}
